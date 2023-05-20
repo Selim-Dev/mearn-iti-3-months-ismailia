f4a72298344baabf56052c9b1ab72affcc14aad9
ali.sleem <ali.sleem@fixedmea.com>
2023-05-20 14:43:35 +0300

    deploy

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 
-mongoose.connect("mongodb+srv://alisleem:vXqrv6Qbmwm79CA0@cluster0.iihrpo0.mongodb.net/express?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://test:r3fsmKkLaZzlOfbp@cluster0.ynjvvh3.mongodb.net/express?retryWrites=true&w=majority")
 .then(()=>console.log('connected to db'))
 .catch((err)=>{
 	console.log(err)
